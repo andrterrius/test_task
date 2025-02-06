d9278c50412040677d8cb986f70682e84e58e10e
andrterrius <chashkin.andreei@mail.ru>
2025-02-06 16:59:56 +0300

    fix cashing form

--- src/main.py
+++ src/main.py
@@ -1,25 +1,35 @@
 import random_generator
 
 from users import users
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request, flash, session, redirect, url_for
 from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key recruto'
 
+@app.after_request
+def preprocess(response):
+    response.headers['location'] = request.referrer
+    return response
+
+
 @app.route('/random/', methods=['GET', 'POST'])
-def random_nonauth():
+def random():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
         if username in users and check_password_hash(users[username]['password'], password):
-            return random_generator.code_generate()
+            session['username'] = username
+            return redirect(url_for('random'))
         flash("Логин или пароль введены неверно", "danger")
+    else:
+        if session.get("username"):
+            session.clear()
+            return random_generator.code_generate()
     return render_template('login.html')
 
 @app.route('/random/nonauth')
-def nonauth_index():
+def random_nonauth():
     return random_generator.code_generate()
 
 if __name__ == '__main__':
